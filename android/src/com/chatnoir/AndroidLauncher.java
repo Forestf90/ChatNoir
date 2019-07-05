013ea14c1fe1b2944b5c8afbf6cc8a2249fb5819
Forestf <michalsliwa96@wp.pl>
2019-07-05 17:42:45 +0200

    Create Project

---
+++ android/src/com/chatnoir/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chatnoir;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chatnoir.ChatNoir;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChatNoir(), config);
+	}
+}
